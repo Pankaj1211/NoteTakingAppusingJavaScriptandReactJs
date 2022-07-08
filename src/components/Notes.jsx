12aba2e8d7407d1c8c0c4bf558e40a5891d4b585
Pankaj1211 <102508823+Pankaj1211@users.noreply.github.com>
2022-07-08 11:49:08 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex porro
+        expedita tenetur id tempore, ea repellendus atque impedit corporis.
+        Officia laboriosam sint ducimus quo optio nisi voluptatibus cupiditate,
+        accusamus, fugiat et rem provident quasi tempora dolore.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
